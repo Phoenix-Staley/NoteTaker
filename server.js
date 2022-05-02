50f0583f06ce0934eb826f8a90541c146203990c
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-01 22:34:49 -0700

    Link server.js to database

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const fs = require("fs");
 const uuid = require("uuid");
 const PORT = 3001;
+const data = require("./db/db.json");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
