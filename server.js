33c21f901984bd69e04fa11fff4906a58f0c4de6
Phoenix Staley <PhoenixStaley121@hotmail.com>
2022-05-02 22:00:44 -0700

    Add PORt support for Heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const uuid = require("uuid");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const data = require("./db/db.json");
 
 const app = express();
